f08154ad5f66660c12f50ccbada2b04f3dfc798e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-16 06:19:20 -0300

    feat: create component Home and add function to get movies from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-  return <div>Home</div>;
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedURL);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando filmes...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <p key={movie.title}>{movie.title}</p>)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
