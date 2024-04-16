cf8ea365ecef3cfcc6024fab98eab0859150f604
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-16 07:18:40 -0300

    feat: send query to search movie

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
+
   return (
     <nav id="navbar">
       <h2>
@@ -11,12 +24,14 @@ const Navbar = () => {
           <BiCameraMovie /> MoviesLib
         </Link>
       </h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
           id="search"
           placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
         />
         <button type="submit">
           <BiSearchAlt2 />
